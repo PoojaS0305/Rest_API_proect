a9f57b278ff195047448d3ff6ee67b21729f9d36
PoojaS0305 <poojasampath0305@gmail.com>
2025-03-17 09:29:29 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask,jsonify
+
+
+todo = Flask('__name__')
+
+@todo.route('/student-list')
+def student_list():
+    students = [
+        {
+            'id': 9,
+            'student_name': 'egg',
+            'age': 20,
+            'email': 'ex@email'
+        },
+        {
+            'id': 2,
+            'student_name': 'horse',
+            'age': 21,
+            'email': 'horsy2@email'
+        },
+        {
+            'id': 7,
+            'student_name': 'gum',
+            'age': 22,
+            'email': 'glue3@email'
+        }
+    ]
+    return jsonify(students)
+
+@todo.route('/student-list/<int:id>')
+def student_by_id(id):
+    return jsonify({"message": f"Student ID received: {id}"})
+
+    return jsonify(student)
+if __name__=='__main__':
+
+        todo.run(
+            host='127.0.0.1',
+            port=5010,
+            debug=True
+        )
